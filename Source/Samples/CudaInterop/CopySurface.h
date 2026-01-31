010ec67a300da3e19a7090921ab69a7d01d329dc
zoloypzuo <zuoyiping76@gmail.com>
2026-02-01 00:09:56 +0800

    Remove Samples Directory and Related CMake Entries
    - Deleted the entire Samples directory, including all sample applications and their associated CMakeLists.txt files.
    - Removed references to the Samples directory in the main CMakeLists.txt to streamline the project structure.
    - This cleanup eliminates unused code and reduces project complexity.

--- Source/Samples/CudaInterop/CopySurface.h
+++
@@ -1,9 +0,0 @@
-#include <cuda_runtime.h>
-
-extern void launchCopySurface(
-    cudaSurfaceObject_t input,
-    cudaSurfaceObject_t output,
-    unsigned int width,
-    unsigned int height,
-    unsigned int format
-);
